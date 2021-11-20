a25ed5b46a7fe9053fe68e8947012524094632f1
TopCodeBeast <maxb47163@gmail.com>
2021-11-20 11:53:44 +0900

    init

---
+++ src/components/rmrk/Gallery/Search/types.ts
@@ -0,0 +1,16 @@
+export type QueryType = Record<string, unknown>
+
+export type SortType = {
+  field: string,
+  value: -1 | 1
+}
+
+export type SortBy = Record<string, number>;
+
+export type SearchQuery = {
+  search: string;
+  type?: string;
+  sortBy?: string;
+  listed: boolean;
+}
+
---
+++ src/components/shared/format/Percent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{value | toPercent }}</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Percent extends Vue {
+  @Prop({ default: 0, type: [String, Number]}) readonly value: number | string | undefined;
+}
+
+</script>
