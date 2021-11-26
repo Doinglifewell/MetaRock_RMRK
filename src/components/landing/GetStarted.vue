a5ff4b20fda4347c69d2dff811d28b7ff4ada6f2
Black_storm <maxb47163@gmail.com>
2021-11-26 16:21:58 +0900

    which one are you?

--- src/components/landing/GetStarted.vue
+++ src/components/landing/GetStarted.vue
@@ -3,10 +3,10 @@
     <div class="hero-body">
       <div class="container">
         <div class="columns is-mobile">
-          <p class="head-text column is-6">Which one are you?</p>
+          <p class="head-text column">Which one are you?</p>
         </div>
         <div class="columns is-mobile">
-          <div class="column is-6">
+          <div class="column">
             <b-button tag="router-link" to="/chooseChain" type="is-inverte" class="mr-3 mt-2">Creater</b-button>
             <b-button tag="router-link" to="/exploreChain" type="is-inverte" class="mr-3 mt-2">Collector</b-button>
           </div>
